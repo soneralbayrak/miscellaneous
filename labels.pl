e2e0a1ebaa99da4c6cc69ffe56fa9da63447fc5f
Soner Albayrak <snralbayrak@gmail.com>
2022-09-08 19:31:47 +0300

    second commit

---
+++ labels.pl
@@ -0,0 +1,13 @@
+# LaTeX2HTML 2022.2 (Released July 1, 2022)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2022.2 (Released July 1, 2022)
+# labels from external_latex_labels array.
+
+
+1;
+
